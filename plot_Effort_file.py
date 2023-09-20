3b09ffd16acb75518104fc4f031a2f82da0e8c03
Théodore MICHEL <theodore.michel@corp.sophia.mines-paristech.fr>
2023-09-20 19:11:58 +0200

    dirty inlet updates

--- plot_Effort_file.py
+++ plot_Effort_file.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import os, sys
 
 ### INPUTS
-NAME   = str(sys.argv[1])
-WINDOW = [ float(sys.argv[2]) , float(sys.argv[3]) ]
+NAME     = str(sys.argv[1])
+WINDOW   = [ float(sys.argv[2]) , float(sys.argv[3]) ]
+WINDOWMA = 100
 
 INCR = []
 TIME = []
@@ -26,6 +27,7 @@ CY   = np.array(CY)
 DT        = TIME[1]-TIME[0]
 WINDOW[1] = min(WINDOW[1],TIME[-1])
 Indexes   = (int(WINDOW[0]/DT),int(WINDOW[1]/DT))
+IndexesMA = (int(WINDOWMA/DT),int(WINDOW[1]/DT))
 
 ### FUNCTION
 def moving_avg(List, window=50):
@@ -33,21 +35,21 @@ def moving_avg(List, window=50):
     for i in range(len(List)):
         MAList.append(np.mean(List[max(0,i-window):i+1]))
     return(MAList)
-CX_ma = moving_avg(CX,50)
-CY_ma = moving_avg(CY,50)
+CX_ma = moving_avg(CX,window=int(WINDOWMA/DT))
+CY_ma = moving_avg(CY,window=int(WINDOWMA/DT))
 
 ### PLOTS
 CX_limits = (min(CX[Indexes[0]:Indexes[1]]), max(CX[Indexes[0]:Indexes[1]]))
 CY_limits = (min(CY[Indexes[0]:Indexes[1]]), max(CY[Indexes[0]:Indexes[1]]))
 # CX
 fig1, ax1  = plt.subplots(figsize=(10,8))
-ax1.plot(TIME[Indexes[0]:Indexes[1]],  CX[Indexes[0]:Indexes[1]],    color = 'lightgray', linewidth = 0.6, label = 'Instant', linestyle='-')
-ax1.plot(TIME[Indexes[0]:Indexes[1]],  CX_ma[Indexes[0]:Indexes[1]], color = 'black',     linewidth = 1, label = 'Moving Average',  linestyle='-')
+ax1.plot(TIME[Indexes[0]:Indexes[1]],      CX[Indexes[0]:Indexes[1]],        color = 'lightgray', linewidth = 0.9, label = 'Instant',         linestyle='-')
+ax1.plot(TIME[IndexesMA[0]:IndexesMA[1]],  CX_ma[IndexesMA[0]:IndexesMA[1]], color = 'black',     linewidth = 1,   label = 'Moving Average',  linestyle='-')
 ax1.legend(loc="lower right", fontsize="large")  # Set the font size to "small"
 ax1.grid(False)# (True, linewidth=0.5, linestyle='--', color='gray')
-ax1.set_title(f'{NAME} Cx')
+ax1.set_title(f'{NAME} $C_x$')
 ax1.set_xlabel('Time (s)')
-ax1.set_ylabel('Cx')
+ax1.set_ylabel('$C_x$')
 ax1.set_xlim((WINDOW[0],WINDOW[1]))
 ax1.set_ylim((CX_limits[0],CX_limits[1]))
 ax1.tick_params(labelright=True, labelleft=True, labeltop=True, left=True, right=True, top=True)
@@ -56,13 +58,13 @@ fig1.savefig(f'{NAME}_CX.png')
 
 # CY
 fig2, ax2  = plt.subplots(figsize=(10,8))
-ax2.plot(TIME[Indexes[0]:Indexes[1]],  CY[Indexes[0]:Indexes[1]],    color = 'lightgray', linewidth = 0.6, label = 'Instant', linestyle='-')
-ax2.plot(TIME[Indexes[0]:Indexes[1]],  CY_ma[Indexes[0]:Indexes[1]], color = 'black',     linewidth = 1, label = 'Moving Average',  linestyle='-')
+ax2.plot(TIME[Indexes[0]:Indexes[1]],      CY[Indexes[0]:Indexes[1]],        color = 'lightgray', linewidth = 0.9, label = 'Instant',        linestyle='-')
+ax2.plot(TIME[IndexesMA[0]:IndexesMA[1]],  CY_ma[IndexesMA[0]:IndexesMA[1]], color = 'black',     linewidth = 1,   label = 'Moving Average', linestyle='-')
 ax2.legend(loc="lower right", fontsize="large")  # Set the font size to "small"
 ax2.grid(False)# (True, linewidth=0.5, linestyle='--', color='gray')
-ax2.set_title(f'{NAME} Cy')
+ax2.set_title(f'{NAME} $C_y$')
 ax2.set_xlabel('Time (s)')
-ax2.set_ylabel('Cy')
+ax2.set_ylabel('$C_y$')
 ax2.set_xlim((WINDOW[0],WINDOW[1]))
 ax2.set_ylim((CY_limits[0],CY_limits[1]))
 ax2.tick_params(labelright=True, labelleft=True, labeltop=True, left=True, right=True, top=True)
