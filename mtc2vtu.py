1c2d4e968441cc38e86d3c556e323de15c3ec9e0
Théodore MICHEL <theodore.michel@corp.sophia.mines-paristech.fr>
2023-10-10 14:25:43 +0200

    efficiency mtc2vtu

--- mtc2vtu.py
+++ mtc2vtu.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-# Check if the user provided the input.t file and VitesseIn.mtc file as command-line arguments
+# Check command-line arguments
 if len(sys.argv) != 3:
     print("Usage: python script.py input.t input.mtc")
     sys.exit(1)
@@ -11,40 +11,34 @@ if len(sys.argv) != 3:
 # Get the input.t and VitesseIn.mtc file names from the command-line arguments
 MESH_FILE = sys.argv[1]
 MTC_FILE  = sys.argv[2]
+VTU_FILE  = 'output.vtu'
 
 # Load the .t mesh file
 with open(MESH_FILE, 'r') as MESH:
     MESH_LINES = MESH.readlines()
 
 # Extract the number of points and cells from the header of the .t mesh file
-num_points = int(MESH_LINES[0].strip().split()[0])
-tot_cells  = int(MESH_LINES[0].strip().split()[2])
-num_cells  = 0
-for line in MESH_LINES[num_points + 2:num_points + 2 + tot_cells]:
-    # Assuming that the cell_line contains 4 integers separated by spaces
-    if line.strip().split()[3] != '0':
-        num_cells += 1
+# num_points = int(MESH_LINES[0].strip().split()[0])
+# tot_cells  = int(MESH_LINES[0].strip().split()[2])
+# num_cells  = 0
+# for line in MESH_LINES[num_points + 2:num_points + 2 + tot_cells]:
+#     # Assuming that the cell_line contains 4 integers separated by spaces
+#     if line.strip().split()[3] != '0':
+#         num_cells += 1
+num_points, num_cells = map(int, MESH_LINES[0].split()[::2])
 
 # Extract the Z coordinates from the mesh file
-z_coordinates = [float(line.strip().split()[2]) for line in MESH_LINES[1:num_points + 1]]
+z_coordinates = np.array([float(line.split()[2]) for line in MESH_LINES[1:num_points + 1]])
 
 # Load the .mtc file to retrieve velocity vectors
 with open(MTC_FILE, 'r') as MTC:
-    MTC_LINES = MTC.readlines()
+    MTC_LINES = MTC.readlines()[3:-1]
 
-# Extract velocity vectors from the .mtc file (skip the first 3 lines)
-velocity_vectors = []
-header_lines = 3  # Number of header lines to skip
-
-for line in MTC_LINES[header_lines:]:
-    if line.strip() == '}':
-        break
-    parts = line.strip().split()
-    velocity_vectors.append([float(parts[0]), float(parts[1]), float(parts[2])])
+# Extract velocity vectors from the .mtc file
+velocity_vectors = np.array([list(map(float, line.split())) for line in MTC_LINES])
 
 # Create a VTU file manually
-vtu_file = 'output.vtu'
-with open(vtu_file, 'w') as vtu:
+with open(VTU_FILE, 'w') as vtu:
     vtu.write('<VTKFile type="UnstructuredGrid" version="0.1" byte_order="LittleEndian">\n')
     vtu.write('<UnstructuredGrid>\n')
     vtu.write(f'<Piece NumberOfPoints="{num_points}" NumberOfCells="{num_cells}">\n')
@@ -54,7 +48,7 @@ with open(vtu_file, 'w') as vtu:
     # Write mesh points
     print("Writing Points")
     for point_line in tqdm(MESH_LINES[1:num_points + 1]):
-        x, y, z = map(float, point_line.strip().split()[:3])
+        x, y, z = map(float, point_line.split()[:3])
         vtu.write(f'{x} {y} {z}\n')
 
     vtu.write('</DataArray>\n')
@@ -66,7 +60,7 @@ with open(vtu_file, 'w') as vtu:
 
     print("Writing Connectivites")
     for cell_line in tqdm(MESH_LINES[num_points + 2:num_points + 2 + num_cells]):          
-        connectivities = list(map(int, cell_line.strip().split()[:4]))
+        connectivities = list(map(int, cell_line.split()[:4]))
         # Subtract 1 from each value in the connectivities
         adjusted_connectivities = [str(val - 1) for val in connectivities]
         vtu.write(' '.join(adjusted_connectivities) + '\n')
@@ -89,7 +83,7 @@ with open(vtu_file, 'w') as vtu:
     vtu.write('<DataArray type="UInt8" Name="types" format="ascii">\n')
 
     print("Writing Types")
-    for cell_line in tqdm(MESH_LINES[num_points + 2:num_points + 2 + num_cells]):
+    for _ in tqdm(range(num_cells)):
         # Assuming cell type 10 (Tetrahedron) for all cells
         vtu.write('10\n')
     vtu.write('</DataArray>\n')
@@ -110,4 +104,4 @@ with open(vtu_file, 'w') as vtu:
     vtu.write('</UnstructuredGrid>\n')
     vtu.write('</VTKFile>\n')
 
-print(f'Generated {vtu_file} successfully.')
+print(f'Generated {VTU_FILE} successfully.')
