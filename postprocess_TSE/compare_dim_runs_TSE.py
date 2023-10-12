138718578534c433f6483591f2a602e3e916162d
Théodore MICHEL <theodore.michel@corp.sophia.mines-paristech.fr>
2023-10-12 16:30:17 +0200

    tse dim run

--- postprocess_TSE/compare_dim_runs_TSE.py
+++ postprocess_TSE/compare_dim_runs_TSE.py
@@ -59,55 +59,58 @@ def compute_std(L, window=[50,100], dt=0.1):
 
 
 ### MESH COMPARISON
-def CompareRuns(ListToCompare, plot_start=5, comparisontype='mesh'):
+def CompareRuns(ListToCompare, plot_start=5, comparisontype='mesh', ListObjects=''):
     colors    = ['darkblue', 'darkgreen', 'darkred', 'darkorange', 'darkorchid','darksalmon','darkmagenta','darkcyan','darkgray','saddlebrown']
-    color_it  = 0
     linestl   = ['dotted','solid','dashed']
-    line_it   = 0
-    figl,axl  = plt.subplots(1,1,figsize=(11,6.5))
-    figd,axd  = plt.subplots(1,1,figsize=(11,6.5))
-    for data in ListToCompare:
-        Lift          = retrieve_lift(f'simu_{data}/resultats/capteurs/') # setups+'/Resultats/'
-        Drag          = retrieve_drag(f'simu_{data}/resultats/capteurs/')
-        SimuTime      = retrieve_time(f'simu_{data}/resultats/capteurs/')  # setups+'/Resultats/'
-        DT            = SimuTime[0]
-        incr_start    = int(plot_start/DT)
-        MeanLift      = []
-        MeanDrag      = []
-        for t in SimuTime[incr_start:]: # plot_start is when to start plotting and computing cumul avg (in s)
-            MeanLift.append(compute_mean(Lift, window=[plot_start,t], dt=DT))
-            MeanDrag.append(compute_mean(Drag, window=[plot_start,t], dt=DT))
-        MeanLift = np.array(MeanLift)
-        MeanDrag = np.array(MeanDrag)
+    # loop on simulations
+    for object in ListObjects:
+        color_it  = 0
+        figl,axl  = plt.subplots(1,1,figsize=(11,6.5))
+        figd,axd  = plt.subplots(1,1,figsize=(11,6.5))
+        # loop on objects (ipe, table, poutre, ...)
+        for data in ListToCompare:
+            # get data
+            Lift          = retrieve_lift(f'simu_{data}/resultats/capteurs/', suffix=object) # setups+'/Resultats/'
+            Drag          = retrieve_drag(f'simu_{data}/resultats/capteurs/', suffix=object)
+            SimuTime      = retrieve_time(f'simu_{data}/resultats/capteurs/', suffix=object)  # setups+'/Resultats/'
+            DT            = SimuTime[0]
+            incr_start    = int(plot_start/DT)
+            MeanLift      = []
+            MeanDrag      = []
+            for t in SimuTime[incr_start:]: # plot_start is when to start plotting and computing cumul avg (in s)
+                MeanLift.append(compute_mean(Lift, window=[plot_start,t], dt=DT))
+                MeanDrag.append(compute_mean(Drag, window=[plot_start,t], dt=DT))
+            MeanLift = np.array(MeanLift)
+            MeanDrag = np.array(MeanDrag)
 
-        if comparisontype=='mesh': labelname = f'{data} mesh'
-        if comparisontype=='dt': labelname = f'$\Delta t = {DT}$'
+            if comparisontype=='mesh': labelname = f'{data} mesh'
+            if comparisontype=='dt': labelname = f'$\Delta t = {DT}$'
 
-        axl.plot(SimuTime[incr_start:], MeanLift, linestyle=linestl[0], color=colors[color_it], label=f'$\overline{{F_z}}$ {labelname}', linewidth=1.3)    
-        axl.plot(SimuTime[incr_start:], Lift[incr_start:],  linestyle=linestl[1], color=colors[color_it], label=f'$F_z$ {labelname}', linewidth=0.5, alpha=0.7) 
+            axl.plot(SimuTime[incr_start:], MeanLift, linestyle=linestl[0], color=colors[color_it], label=f'$\overline{{F_z}}$ {labelname}', linewidth=1.3)    
+            axl.plot(SimuTime[incr_start:], Lift[incr_start:],  linestyle=linestl[1], color=colors[color_it], label=f'$F_z$ {labelname}', linewidth=0.5, alpha=0.7) 
 
-        axd.plot(SimuTime[incr_start:], MeanDrag, linestyle=linestl[0], color=colors[color_it], label=f'$\overline{{F_x}}$ {labelname}', linewidth=1.3)    
-        axd.plot(SimuTime[incr_start:], Drag[incr_start:], linestyle=linestl[1], color=colors[color_it], label=f'$F_x$ {labelname}', linewidth=0.5, alpha=0.7) 
+            axd.plot(SimuTime[incr_start:], MeanDrag, linestyle=linestl[0], color=colors[color_it], label=f'$\overline{{F_x}}$ {labelname}', linewidth=1.3)    
+            axd.plot(SimuTime[incr_start:], Drag[incr_start:], linestyle=linestl[1], color=colors[color_it], label=f'$F_x$ {labelname}', linewidth=0.5, alpha=0.7) 
 
-        color_it += 1
+            color_it += 1
 
-    handles, labels = axl.get_legend_handles_labels()
-    axl.grid(True, linewidth=0.5, linestyle='--', color='gray')
-    axl.set(xlabel='Time (s)', ylabel='$F_z$ (N)', xlim=(plot_start,SimuTime[-1])) # xlim=(plot_start,SimuTime[-1])
-    axl.tick_params(labelright=True, labelleft=True, left=True, right=True)
-    axl.legend(handles, labels, loc = 'lower center', ncol=3+(n_args-2)%3, fontsize='small')
-    figl.suptitle(f'$F_z$ for different {comparisontype} resolutions - H3 IPE amont $\Theta=-5^{{\circ}}$ $V=50km.h^{{-1}}$')
-    figl.tight_layout()
-    figl.savefig(f'{comparisontype}_LIFT_COMP_PLOT.png')
+        handles, labels = axl.get_legend_handles_labels()
+        axl.grid(True, linewidth=0.5, linestyle='--', color='gray')
+        axl.set(xlabel='Time (s)', ylabel='$F_z$ (N)', xlim=(plot_start,SimuTime[-1])) # xlim=(plot_start,SimuTime[-1])
+        axl.tick_params(labelright=True, labelleft=True, left=True, right=True)
+        axl.legend(handles, labels, loc = 'lower center', ncol=3+(n_args-2)%3, fontsize='small')
+        figl.suptitle(f'{object} $F_z$ for different {comparisontype} resolutions - H3 IPE amont $\Theta=-5^{{\circ}}$ $V=50km.h^{{-1}}$')
+        figl.tight_layout()
+        figl.savefig(f'{comparisontype}_{object}_LIFT_COMP_PLOT.png')
 
-    handles, labels = axd.get_legend_handles_labels()
-    axd.grid(True, linewidth=0.5, linestyle='--', color='gray')
-    axd.set(xlabel='Time (s)', ylabel='$F_x$ (N)', xlim=(plot_start,SimuTime[-1])) # xlim=(plot_start,SimuTime[-1])
-    axd.tick_params(labelright=True, labelleft=True, left=True, right=True)
-    axd.legend(handles, labels, loc = 'lower center', ncol=3+(n_args-2)%3, fontsize='small')
-    figd.suptitle(f'$F_x$ for different {comparisontype} resolutions - H3 IPE amont $\Theta=-5^{{\circ}}$ $V=50km.h^{{-1}}$')
-    figd.tight_layout()
-    figd.savefig(f'{comparisontype}_DRAG_COMP_PLOT.png')
+        handles, labels = axd.get_legend_handles_labels()
+        axd.grid(True, linewidth=0.5, linestyle='--', color='gray')
+        axd.set(xlabel='Time (s)', ylabel='$F_x$ (N)', xlim=(plot_start,SimuTime[-1])) # xlim=(plot_start,SimuTime[-1])
+        axd.tick_params(labelright=True, labelleft=True, left=True, right=True)
+        axd.legend(handles, labels, loc = 'lower center', ncol=3+(n_args-2)%3, fontsize='small')
+        figd.suptitle(f'{object} $F_x$ for different {comparisontype} resolutions - H3 IPE amont $\Theta=-5^{{\circ}}$ $V=50km.h^{{-1}}$')
+        figd.tight_layout()
+        figd.savefig(f'{comparisontype}_{object}_DRAG_COMP_PLOT.png')
     
     return()
 
@@ -153,5 +156,5 @@ plt.style.use(scientific_style)
 # CompareRuns(LIST_FOR_COMPARISON, PLOT_START, comparisontype='dt')
 # CompareRuns(LIST_FOR_COMPARISON, PLOT_START, comparisontype='dt', ListObjects=LIST_OF_OBJECTS)
 # CompareRuns(LIST_FOR_COMPARISON, PLOT_START, comparisontype='mesh')
-# CompareRuns(LIST_FOR_COMPARISON, PLOT_START, comparisontype='mesh', ListObjects=LIST_OF_OBJECTS)
-PrintEvaluation(LIST_FOR_COMPARISON, OUTPUT_CSV, PLOT_START, comparisontype='mesh', ListObjects=LIST_OF_OBJECTS)
+CompareRuns(LIST_FOR_COMPARISON, PLOT_START, comparisontype='mesh', ListObjects=LIST_OF_OBJECTS)
+# PrintEvaluation(LIST_FOR_COMPARISON, OUTPUT_CSV, PLOT_START, comparisontype='mesh', ListObjects=LIST_OF_OBJECTS)
